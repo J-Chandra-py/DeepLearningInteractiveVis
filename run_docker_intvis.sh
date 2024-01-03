c2e7d62d48ad841be2a6f4c099092986efd1b206
Jayachandra <100439113+J-Chandra-py@users.noreply.github.com>
2024-01-03 23:31:10 +0100

    Revert "Updated documentation"
    This reverts commit d89ef2228b75c6e067446ed02deb36fba5fb7219.

--- run_docker_intvis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# toggle command line options if required:
-# -t   to display output in current terminal, without input
-# -it  to run interactively with option to enter input (but then also remove the ampersand at the end)
-docker run -t --name interactivevis -p 5006:5006 martindyrba/interactivevis:latest &
